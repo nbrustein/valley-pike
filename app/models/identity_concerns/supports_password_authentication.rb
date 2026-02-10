89ceed3de3acb020fc00344a43b45779cf7929f3
Nate Brustein <nbrustein@gmail.com>
2026-02-10 08:26:26 -0500

    global frozen strings (#19)

--- app/models/identity_concerns/supports_password_authentication.rb
+++ app/models/identity_concerns/supports_password_authentication.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module IdentityConcerns
   module SupportsPasswordAuthentication
     extend ActiveSupport::Concern
--- spec/requests/requests_spec_helper.rb
+++ spec/requests/requests_spec_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module RequestsSpecHelper
   DEFAULT_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
