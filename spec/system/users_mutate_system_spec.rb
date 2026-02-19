cd495eb773bb25016be0c3742bab80b5df420104
Nate Brustein <nbrustein@gmail.com>
2026-02-19 14:10:24 -0500

    a system spec

---
+++ spec/system/users_mutate_system_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+require "warden/test/helpers"
+
+RSpec.describe "User create form", type: :system, js: true do
+  include Warden::Test::Helpers
+
+  let(:organization) { create(:organization, name: "UDO Org", abbreviation: "UDO") }
+  let(:identity) do
+    user = create(:user, email: "udo-admin@example.com")
+    create(:user_role, user:, role: UserRole::DEVELOPER)
+    create(:identity, :magic_link, user:, email: user.email)
+  end
+
+  before do
+    login_as(identity, scope: :identity)
+  end
+
+  after do
+    Warden.test_reset!
+  end
+
+  describe "user create form" do
+    context "when a global role is selected that grants org admin privileges" do
+      it "leaves Organization Roles section hidden" do
+        visit new_user_path
+        choose "Vanita admin"
+        expect(page).to have_css("[data-organization-roles]", visible: :hidden)
+      end
+    end
+
+    context "when a global role is selected that does not grant org admin privileges" do
+      it "reveals Organization Roles section" do
+        visit new_user_path
+        choose "Vanita viewer"
+        expect(page).to have_css("[data-organization-roles]", visible: true)
+      end
+    end
+  end
+end
