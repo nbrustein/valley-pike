9161195fe3ef5eee21622260745e4def9137fe5e
Nate Brustein <nbrustein@gmail.com>
2026-02-19 22:29:43 -0500

    whitespace

--- spec/system/users_mutate_system_spec.rb
+++ spec/system/users_mutate_system_spec.rb
@@ -18,45 +18,38 @@ RSpec.describe "User create form", type: :system, js: true do
     Warden.test_reset!
   end
 
-  describe "user create form" do
-    context "when a global role is selected that grants org admin privileges" do
-      it "leaves Organization Roles section hidden" do
-        visit new_user_path
-        choose "Vanita admin"
-        expect_org_admin_user_role_inputs(:hidden)
-      end
+  context "when a global role is selected that grants org admin privileges" do
+    it "leaves Organization Roles section hidden" do
+      visit new_user_path
+      choose "Vanita admin"
+      expect_org_admin_user_role_inputs(:hidden)
+    end
+  end
+
+  context "when a global role is selected that does not grant org admin privileges" do
+    let!(:organization) { create(:organization, name: "UDO Org", abbreviation: "UDO") }
+    let!(:other_organization) { create(:organization, name: "VDO Org", abbreviation: "VDO") }
+    let(:email) { "ride.requester@example.com" }
+
+    it "reveals Organization Roles section which can be used to select an org admin role" do
+      visit new_user_path
+      act
+      expect(page).to have_current_path(users_path)
+      expect_user_to_have_roles(email, [ [ UserRole::RIDE_REQUESTER, organization.id ] ])
     end
 
-    context "when a global role is selected that does not grant org admin privileges" do
-      let!(:organization) { create(:organization, name: "UDO Org", abbreviation: "UDO") }
-      let!(:other_organization) { create(:organization, name: "VDO Org", abbreviation: "VDO") }
-      let(:email) { "ride.requester@example.com" }
+    def act
+      fill_in "Email", with: email
 
-      it "reveals Organization Roles section which can be used to select an org admin role" do
-        visit new_user_path
-        act
-        expect(page).to have_current_path(users_path)
-        expect_user_to_have_roles(email, [ [ UserRole::RIDE_REQUESTER, organization.id ] ])
+      # select a global role
+      choose "None"
+      expect_org_admin_user_role_inputs(:visible)
+
+      # select an org admin role
+      within_org_admin_row(organization.name) do
+        select_radio_with_value("ride_requester")
       end
-
-      def act
-        fill_in "Email", with: email
-
-        # select a global role
-        choose "None"
-        expect_org_admin_user_role_inputs(:visible)
-
-        # select an org admin role
-        within_org_admin_row(organization.name) do
-          select_radio_with_value("ride_requester")
-        end
-        click_button "Create ride requester"
-      end
-    end
-
-    def expect_org_admin_user_role_inputs(visibility)
-      visible = visibility == :visible ? true : false
-      expect(page).to have_css("[data-org-admin-user-role-inputs]", visible:)
+      click_button "Create ride requester"
     end
 
     def within_org_admin_row(organization_name, &block)
@@ -72,5 +65,10 @@ RSpec.describe "User create form", type: :system, js: true do
       expect(user).to be_present
       expect(user.user_roles.map {|ur| [ ur.role, ur.organization_id ] }).to eq(role_pairs)
     end
+  end
+
+  def expect_org_admin_user_role_inputs(visibility)
+    visible = visibility == :visible ? true : false
+    expect(page).to have_css("[data-org-admin-user-role-inputs]", visible:)
   end
 end
