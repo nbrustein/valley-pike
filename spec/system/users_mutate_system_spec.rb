d29a767e68060c809832ab6e9d159999af63efce
Nate Brustein <nbrustein@gmail.com>
2026-02-19 21:42:40 -0500

    working on form

--- spec/system/users_mutate_system_spec.rb
+++ spec/system/users_mutate_system_spec.rb
@@ -23,27 +23,53 @@ RSpec.describe "User create form", type: :system, js: true do
       it "leaves Organization Roles section hidden" do
         visit new_user_path
         choose "Vanita admin"
-        expect(page).to have_css("[data-org-admin-user-role-inputs]", visible: :hidden)
+        expect_org_admin_user_role_inputs(:hidden)
       end
     end
 
     context "when a global role is selected that does not grant org admin privileges" do
-      let(:organization!) { create(:organization, name: "UDO Org", abbreviation: "UDO") }
+      let!(:organization) { create(:organization, name: "UDO Org", abbreviation: "UDO") }
       let!(:other_organization) { create(:organization, name: "VDO Org", abbreviation: "VDO") }
+      let(:email) { "ride.requester@example.com" }
 
-      it "reveals Organization Roles section" do
+      it "reveals Organization Roles section which can be used to select an org admin role" do
         visit new_user_path
-        choose "None"
-
-        expect(page).to have_css("[data-org-admin-user-role-inputs]", visible: true)
-        
-
-        first("[data-org-role-input]").click
-        fill_in "Email", with: "ride.requester@example.com"
-        click_button "Create ride requester"
-
-        expect(page).to have_current_path("/users")
+        act
+        expect_user_to_have_roles(email, [ [ UserRole::RIDE_REQUESTER, organization.id ] ])
       end
+
+      def act
+        fill_in "Email", with: email
+
+        # select a global role
+        choose "None"
+        expect_org_admin_user_role_inputs(:visible)
+
+        # select an org admin role
+        within_org_admin_row(organization.name) do
+          select_radio_with_value("ride_requester")
+        end
+        click_button "Create ride requester"
+      end
+    end
+
+    def expect_org_admin_user_role_inputs(visibility)
+      visible = visibility == :visible ? true : false
+      expect(page).to have_css("[data-org-admin-user-role-inputs]", visible:)
+    end
+
+    def within_org_admin_row(organization_name, &block)
+      within(:xpath, "//tr[td[contains(., '#{organization_name}')]]", &block)
+    end
+
+    def select_radio_with_value(value)
+      find("input[type='radio'][value='#{value}']").click
+    end
+
+    def expect_user_to_have_roles(email, role_pairs)
+      user = User.find_by(email:)
+      expect(user).to be_present
+      expect(user.user_roles.map {|ur| [ ur.role, ur.organization_id ] }).to eq(role_pairs)
     end
   end
 end
