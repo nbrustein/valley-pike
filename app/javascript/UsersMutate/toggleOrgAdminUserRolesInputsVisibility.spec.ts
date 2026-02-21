dbef006c7c6b355e63c47e24ef7532558cfd4cb1
Nate Brustein <nbrustein@gmail.com>
2026-02-20 22:15:36 -0500

    jest and lint

---
+++ app/javascript/UsersMutate/toggleOrgAdminUserRolesInputsVisibility.spec.ts
@@ -0,0 +1,18 @@
+describe("toggleOrgAdminUserRolesInputsVisibility", () => {
+    describe('when the global role input is initially unchecked', () => {
+        it('hides the organization roles inputs element', () => {
+            flunk();
+        });
+        it('shows the organization roles inputs element when the global role input is checked', () => {
+            flunk();
+        });
+    });
+    describe('when the global role input is initially checked', () => {
+        it('shows the organization roles inputs element', () => {
+            flunk();
+        });
+        it('hides the organization roles inputs element when the global role input is unchecked', () => {
+            flunk();
+        });
+    });
+});
