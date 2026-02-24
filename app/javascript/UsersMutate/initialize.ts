4581019650e3a5aa888c16e25679c65fe47e51a3
Nate Brustein <nbrustein@gmail.com>
2026-02-23 21:57:13 -0500

    driver qualification

--- app/javascript/UsersMutate/initialize.ts
+++ app/javascript/UsersMutate/initialize.ts
@@ -1,18 +1,24 @@
 import { toggleOrgAdminUserRolesInputsVisibility } from "./toggleOrgAdminUserRolesInputsVisibility";
 import { syncPreferredName } from "./syncPreferredName";
+import { toggleDriverQualificationInputs } from "./toggleDriverQualificationInputs";
 
 type Inputs = {
     organizationRolesElement: HTMLElement;
     globalRoleInputElement: NodeListOf<HTMLInputElement>;
     fullNameInputElement: HTMLInputElement | null;
     preferredNameInputElement: HTMLInputElement | null;
+    driverRoleInputElement: HTMLInputElement | null;
+    driverQualificationInputElements: NodeListOf<HTMLInputElement>;
 }
 export function initialize({
     organizationRolesElement,
     globalRoleInputElement,
     fullNameInputElement,
     preferredNameInputElement,
+    driverRoleInputElement,
+    driverQualificationInputElements,
 }: Inputs) {
     toggleOrgAdminUserRolesInputsVisibility({ organizationRolesElement, globalRoleInputElement })
     syncPreferredName({ fullNameInputElement, preferredNameInputElement })
+    toggleDriverQualificationInputs({ driverRoleInputElement, driverQualificationInputElements })
 }
