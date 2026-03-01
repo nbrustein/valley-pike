991f1b82f2572c891c37f2d920d50636008ab0a4
Nate Brustein <nbrustein@gmail.com>
2026-03-01 12:17:05 -0500

    Parallel specs (#37)
    * New roles and role validations
    
    * working on Restrict org admins to viewing ride requesters
    
    * Restrict org admins to viewing ride requesters
    
    * clean up policy
    
    * working on Enable org admins to create ride requesters
    
    * fix users index label
    
    * working on Enable org admins to create ride requesters
    
    * Agents
    
    * Enable org admins to create ride requesters
    
    * user_index_policy and controller
    
    * back to pundit
    
    * wokring on refactor the user manage stuff
    
    * working on handling roles in user forms
    
    * global role
    
    * a system spec
    
    * showing org admin inputs
    
    * working on form
    
    * wip
    
    * org admin field specs
    
    * Fixed system spec
    
    * whitespace
    
    * failing test
    
    * test for creating ride requester
    
    * typescript
    
    * jest and lint
    
    * jest fixes
    
    * still trying to get jest running in ci
    
    * fill in jest spec
    
    * focus user mutate system spec now that we have some jest coverage
    
    * change default current user roles in mutate system spec
    
    * edit human
    
    * stop running yarn install on every test run
    
    * fix app  menu spec
    
    * spec fix
    
    * reorganize user mutate spec
    
    * create driver
    
    * workign on system specs
    
    * driver test
    
    * done moving stuff from request spec to system spec
    
    * driver qualifications
    
    * driver qualification
    
    * refactor some specs
    
    * edit
    
    * fix form action
    
    * pre-fill roles
    
    * run tests in parallel

--- db/migrate/20260221121500_update_humans_for_preferred_name.rb
+++ db/migrate/20260221121500_update_humans_for_preferred_name.rb
@@ -10,21 +10,17 @@ class UpdateHumansForPreferredName < ActiveRecord::Migration[8.1]
     change_column_default :humans, :preferred_name, nil
 
     remove_column :humans, :sortable_name, :string
-    remove_column :humans, :given_name, :text
   end
 
   def down
     add_column :humans, :sortable_name, :string, null: false, default: ""
-    add_column :humans, :given_name, :text, null: false, default: ""
 
     execute <<~SQL.squish
       UPDATE humans
-      SET sortable_name = full_name,
-          given_name = full_name
+      SET sortable_name = full_name
     SQL
 
     change_column_default :humans, :sortable_name, nil
-    change_column_default :humans, :given_name, nil
 
     remove_column :humans, :preferred_name, :text
   end
