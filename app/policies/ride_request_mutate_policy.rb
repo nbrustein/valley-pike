600ab62abe59c8307ac52d1bace5346268d76e20
Nate Brustein <nbrustein@gmail.com>
2026-03-07 10:49:23 -0500

    ride request mutate policy (#50)

---
+++ app/policies/ride_request_mutate_policy.rb
@@ -0,0 +1,22 @@
+class RideRequestMutatePolicy < ApplicationPolicy
+  include IsOrganizationAwarePolicy
+
+  memoize def new?
+    user&.has_role_permissions?(UserRole::RIDE_REQUESTER) || false
+  end
+
+  def create?
+    return false unless new?
+    return false unless uow
+
+    permitted_organization_ids.include?(uow.organization_id)
+  end
+
+  def uow
+    record.is_a?(UnitOfWork) ? record : nil
+  end
+
+  memoize def roles_granting_org_permissions
+    user&.roles_with_permissions(UserRole::RIDE_REQUESTER) || []
+  end
+end
